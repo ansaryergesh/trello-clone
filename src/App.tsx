de0354f9d8828894c9ca3c1154a8111755de576a
ansaryergesh <yergeshovansar@gmail.com>
2020-08-21 00:28:44 +0600

    reducer lists

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import { Column } from "./Column";
 import { Card } from "./Card";
 import { AddNewItem } from "./AddNewItem";
 import { AppContainer } from "./styles";
+import { useAppState } from "./AppStateContext";
+interface State {
+  count: number;
+}
 
 const App = () => {
+  const { state } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing" />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} />
+      ))}
       <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   );
